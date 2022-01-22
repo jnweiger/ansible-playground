675b1134738dccea5f040196b712a7e131138157
Juergen Weigert <jnweiger@gmail.com>
2022-01-22 02:02:03 +0100

    inkscape 1.1 installation works.

--- RUN.sh
+++ RUN.sh
@@ -3,4 +3,11 @@
 bash docker-test-dummy.sh
 ipaddr=$(docker inspect ansible-test-dummy -f "{{ .NetworkSettings.IPAddress }}")
 ansible-playbook bootstrap.yml -i "$ipaddr," -u root
+ansible-playbook  inkscape.yml -i "$ipaddr," -u root
 
+# should be version 1.1
+ssh root@$ipaddr inkscape --version
+
+echo "Press Enter to kill docker container ansible-test-dummy"
+read a
+docker kill ansible-test-dummy
