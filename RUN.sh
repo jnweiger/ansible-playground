1fb3efa3011e5288a9fc11c1b5b268c310ebf743
Juergen Weigert <jnweiger@gmail.com>
2022-01-24 03:19:58 +0100

    finally: added enough foofoo, so that it works with mint. That was horrible.

--- RUN.sh
+++ RUN.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 # Study https://docs.ansible.com/ansible/latest/network/getting_started/first_inventory.html#basic-inventory
+# Use a quadruple -vvvv to see the stderr and stdout of internal commands.
 
 bash docker-test-dummy.sh
 ipaddr=$(docker inspect ansible-test-dummy -f "{{ .NetworkSettings.IPAddress }}")
 ansible-playbook bootstrap.yml -i "$ipaddr,"
-ansible-playbook  inkscape.yml -i "$ipaddr,"
+ansible-playbook  inkscape.yml -i "$ipaddr,"	# -vvvv
 
 # should be version 1.1
 ssh root@$ipaddr inkscape --version
